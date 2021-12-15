b0d3469d02ffce5baace3109021a1e8c64a6868f
Mansi Ganar <86571822+MansiGanar@users.noreply.github.com>
2021-12-15 02:37:26 +0100

    Revert "GET POST methods are working"
    This reverts commit b30d7549a403818c4b174c991d909bb2b6a5e064.

--- src/services/product/route.js
+++ src/services/product/route.js
@@ -3,24 +3,16 @@ import pool from "../../utils/db/connect.js";
 
 const productRouter = Router();
 
-productRouter.get("/", async (req, res, next) => {
-  try {
-    const result = await pool.query("SELECT * FROM product");
-    res.status(201).send(result.rows);
-  } catch (error) {
-    res.status(500).send({ message: error.message });
-  }
-  next(error);
-});
+productRouter.get("/", async (req, res, next) => {});
 
 productRouter.post("/", async (req, res, next) => {
   try {
     const { name, description, brand, image_url, price, category } = req.body;
     const result = await pool.query(
-      "INSERT INTO product(name , description ,brand , image_url , price , category ) VALUES ($1,$2,$3,$4,$5,$6 )",
+      'INSERT INTO product(name , description ,brand , image_url , price , category ) VALUES ("3222", "something longer", "nokia","https://images.unsplash.com/photo-1639501838991-215ffc3cf280?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60",500, "Mobile" RETURNING * )',
       [name, description, brand, image_url, price, category]
     );
-    res.status(201).send(result.rows[0]);
+    res.status(201).send(result.rows);
   } catch (error) {
     res.status(500).send({ message: error.message });
   }
