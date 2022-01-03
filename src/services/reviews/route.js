01280418059dd56f12278b5d4ca241eeeb076594
Mansi Ganar <86571822+MansiGanar@users.noreply.github.com>
2022-01-03 17:41:15 +0100

    methods added to reviews route

--- src/services/reviews/route.js
+++ src/services/reviews/route.js
@@ -3,13 +3,21 @@ import pool from "../../utils/db/connect.js";
 
 const reviewsRouter = Router();
 
-reviewsRouter.get("/", async (req, res, next) => {});
+reviewsRouter.get("/", async (req, res, next) => {
+  try {
+    const { comment, rate, product_id } = req.body;
+    const result = await pool.query(`SELECT * FROM reviews ;`);
+    res.send(result.rows);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
 
 reviewsRouter.post("/", async (req, res, next) => {
   try {
     const { comment, rate, product_id } = req.body;
     const result = await pool.query(
-      'INSERT INTO product(comment,rate,product_id ) VALUES ("A good product.", 3,1  RETURNING * )',
+      `INSERT INTO reviews(comment,rate,product_id ) VALUES ($1 , $2 , $3) RETURNING * `,
       [comment, rate, product_id]
     );
     res.send(201).send(result.rows);
@@ -17,7 +25,40 @@ reviewsRouter.post("/", async (req, res, next) => {
     res.status(500).send({ message: error.message });
   }
 });
-reviewsRouter.get("/:id", async (req, res, next) => {});
-reviewsRouter.put("/:id", async (req, res, next) => {});
-reviewsRouter.delete("/:id", async (req, res, next) => {});
+reviewsRouter.get("/:id", async (req, res, next) => {
+  try {
+    const { comment, rate, product_id } = req.body;
+    const result = await pool.query(`SELECT * FROM reviews WHERE id = $1 ;`, [
+      req.params.id,
+    ]);
+    if (result.rows[0]) {
+      res.send(result.rows);
+    } else {
+      res.status(404).send(`Reviews with id ${req.params.id} not found`);
+    }
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
+reviewsRouter.put("/:id", async (req, res, next) => {
+  try {
+    const updateStatement = Object.entries(req.body)
+      .map(([key, value]) => `${key} = '${value}'`)
+      .join(", ");
+    const query = `UPDATE reviews SET ${updateStatement} WHERE id = ${req.params.id} RETURNING *; `;
+    const result = await pool.query(query);
+    res.status(201).send(result.rows);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
+reviewsRouter.delete("/:id", async (req, res, next) => {
+  try {
+    const query = `DELETE FROM reviews WHERE id = ${req.params.id} RETURNING *; `;
+    await pool.query(query);
+    res.status(204).send();
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
 export default reviewsRouter;
